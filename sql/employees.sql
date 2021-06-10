38200169b4e845ce2752675c4c00e2325190087f
chdonovan <chrisdonovan311@gmail.com>
2021-06-10 17:52:18 -0500

    employee table added

--- sql/employees.sql
+++ sql/employees.sql
@@ -1,4 +1,5 @@
-/* database setup */
+/* database setup *id UNSIGNED AUTO_INCREMENT PRIMARY KEY, */
+
 DROP DATABASE IF EXISTS  employees;
 CREATE DATABASE employees;
 
@@ -15,8 +16,22 @@ CREATE TABLE role (
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
     department_id INT UNSIGNED NOT NULL,
-    INDEX dep_id (department_id),
+    INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
-);    
+);
+/* EMPLOYEE table */
+ CREATE TABLE employee (
+    id UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) UNIQUE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_ind (role_id, 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+);   
+
     
 
