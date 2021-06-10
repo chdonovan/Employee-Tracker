58938f4187c2bc233f3200ef09afb297fbaf9f15
chdonovan <chrisdonovan311@gmail.com>
2021-06-10 17:35:56 -0500

    role table

--- sql/employees.sql
+++ sql/employees.sql
@@ -9,5 +9,14 @@ CREATE TABLE department (
     name VARCHAR(30) UNIQUE NOT NULL
 
 );
-
+/* ROLE table */
+CREATE TABLE role (
+    id UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_id (department_id),
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
+);    
+    
 
