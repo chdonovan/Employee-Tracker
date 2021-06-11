21d1f7ff2d6e3defe67e73214b16d06b5bce6d26
chdonovan <chrisdonovan311@gmail.com>
2021-06-10 19:51:13 -0500

    viewAllEmployees function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const { and } = require('sequelize/types');
+const Prompt = require('inquirer/lib/prompts/base');
 require('console.table');
 
 const promptMessages = {
@@ -93,3 +94,19 @@ function prompt( ) {
 
 }
 
+function viewAllEmployees() {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role.id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+    connection.query(query, (err, res)=> {
+        if (err) throw err;
+        console.log (err);
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('/n');
+        console.log(res);
+        prompt();
+    });
+}
