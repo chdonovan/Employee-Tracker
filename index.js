30ac90ca33f7ffea6424bb8ab97df196c04a1a2e
chdonovan <chrisdonovan311@gmail.com>
2021-06-10 21:29:22 -0500

    addEmployee function

--- index.js
+++ index.js
@@ -159,3 +159,69 @@ function viewAllRoles(){
     });
 
 }
+ async function addEmployee() {
+    const addname = await inquirer.prompt(askName());
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async (err, res)=>{
+        if (err) throw err;
+        const {role} = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employee role?'
+            }
+        ]);
+        let roleId;
+        for (const row of res){
+            if (row.title === role) {
+                roleID = row.id;
+                continue;
+            }
+        }
+        connection.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res `${res.first_name} ${res.last+name}`);
+            choices.push('none');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'manager', 
+                    type: 'list', 
+                    choices: choices,
+                    message: 'choose the emloyee Manager: '
+                }
+            ]);
+            let managerId;
+            let managerName;
+            if (manager === 'none') {
+                managerId = null;
+
+            } else {
+                for ( const data of res) {
+                    data.fullName = `${data.first_name} ${data.last_name}`;
+                    if (data.fullName === manager) {
+                        managerId = data.id;
+                        managerName = data.fullName;
+                        console.log(managerId);
+                        console.log(managerName);
+                        continue;
+                    }
+                }
+            }
+            console.log('Employee has been added. PLease view all employee to verify');
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: addname.first,
+                    last_name: addname.last,
+                    role_id: parseInt(managerId)
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    prompt();
+                }
+            );
+            
+        });
+    });    
+}
+
