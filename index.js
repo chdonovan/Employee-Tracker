d02c0bb6cd5efe1b1bd180d2495f97962a5f0c24
chdonovan <chrisdonovan311@gmail.com>
2021-06-10 19:41:18 -0500

    prompt msgs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const { and } = require('sequelize/types');
 require('console.table');
 
 const promptMessages = {
@@ -39,6 +40,56 @@ function prompt( ) {
     inquirer
     .prompt({
         name: 'action',
-        
+        type: 'list',
+        message: 'what would you like to do?',
+        choices: [
+            promptMessages.viewAllEmployees,
+            promptMessages.viewByDepartment,
+            promptMessages.viewByManager,
+            promptMessages.viewAllRoles,
+            promptMessages.addEmployee, 
+            promptMessages.removeEmployee,
+            promptMessages.updateRole,
+            promptMessages.exit
+        ]
     })
+    .then( answer => {
+        console.log('answer', answer);
+        switch (answer.action) {
+            case promptMessages.viewAllEmployees:
+                viewAllEmployees();
+                break;
+            
+            case promptMessages.viewByDepartment:
+                viewByDepartment();
+                break;
+            
+            case promptMessages.viewByManager:
+                viewByManager();
+                break;
+            
+            case promptMessages.addEmployee:
+                addEmployee();
+                break;
+            
+            case promptMessages.removeEmployee:
+                remove('delete');
+                break;
+
+            case promptMessages.updateRole:
+                remove('role');
+                break;
+            
+            case promptMessages.viewAllRoles:
+                viewAllRoles();
+                break;
+            
+            case promptMessages.exit:
+                connection.end();
+                break;
+        }
+
+    });
+
 }
+
